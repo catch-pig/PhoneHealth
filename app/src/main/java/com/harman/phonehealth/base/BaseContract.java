af41f2365bf025e82f9ecaf5090fe920c357513d
JHe4 <123>
2019-11-08 10:33:46 +0800

    recycleView列表

--- app/src/main/java/com/harman/phonehealth/base/BaseContract.java
+++ app/src/main/java/com/harman/phonehealth/base/BaseContract.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.OnLifecycleEvent;
 
 public interface BaseContract {
     interface View {
+        BaseActivity getBaseActivity();
         void closeActivity();
     }
 
---
+++ app/src/main/java/com/harman/phonehealth/base/fragment/BaseFragment.java
@@ -0,0 +1,63 @@
+package com.harman.phonehealth.base.fragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import androidx.annotation.CallSuper;
+import androidx.annotation.LayoutRes;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.harman.phonehealth.base.BaseActivity;
+import com.harman.phonehealth.base.BaseContract;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * 创建时间:2019/4/4 23:14<br/>
+ * 创建人: 李涛<br/>
+ * 修改人: 李涛<br/>
+ * 修改时间: 2019/4/4 23:14<br/>
+ * 描述:
+ */
+public abstract class BaseFragment extends Fragment implements BaseContract.View {
+    private Unbinder mUnbinder;
+    private BaseActivity mBaseActivity;
+
+    @CallSuper
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(contentView(),container,false);
+        mUnbinder = ButterKnife.bind(this,view);
+        return view;
+    }
+
+    @LayoutRes
+    protected abstract int contentView();
+
+
+    @Override
+    public BaseActivity getBaseActivity() {
+        if (mBaseActivity==null) {
+            Activity activity = getActivity();
+            if (activity instanceof BaseActivity) {
+                mBaseActivity = (BaseActivity) activity;
+            }
+        }
+        return mBaseActivity;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mUnbinder!=null) {
+            mUnbinder.unbind();
+        }
+    }
+}
