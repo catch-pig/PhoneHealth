e5cd0429212332721ea06a52846ba60c2d00b4f2
RZhou2 <362129663@qq.com>
2019-11-08 11:31:39 +0800

    setbase

--- app/src/main/java/com/harman/phonehealth/database/dao/PackageInfoDao.java
+++ app/src/main/java/com/harman/phonehealth/database/dao/PackageInfoDao.java
@@ -10,28 +10,31 @@ import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
+
+import io.reactivex.Completable;
 import io.reactivex.Flowable;
+import io.reactivex.Single;
 
 @Dao
 public interface PackageInfoDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    public void insertPackageInfos(PackageInfo... publicKeys);
+    Completable insertPackageInfos(List<PackageInfo> publicKeys);
     @Update
-    public void updatePackageInfos(PackageInfo... publicKeys);
+    Completable updatePackageInfos(List<PackageInfo> publicKeys);
     @Delete
-    public void deletePackageInfos(PackageInfo... publicKeys);
+    Completable deletePackageInfos(List<PackageInfo> publicKeys);
     @Query("SELECT * FROM PackageInfo")
-    Flowable <List<PackageInfo>> findPackageInfoAll();
+    Single <List<PackageInfo>> findPackageInfoAll();
 
     @Query("SELECT * FROM PackageInfo WHERE usedCount =:search")
-    Flowable <List<PackageInfo>> findPackageInfoWithUsedCount(int search);
+    Single<List<PackageInfo>> findPackageInfoWithUsedCount(int search);
 
     @Query("SELECT * FROM PackageInfo WHERE usedTime =:search")
-    Flowable <List<PackageInfo>> findPackageInfoWithUserTime(long search);
+    Single <List<PackageInfo>> findPackageInfoWithUserTime(long search);
 
     @Query("SELECT * FROM PackageInfo WHERE packageName =:search")
-    Flowable <List<PackageInfo>> findPackageInfoWithPackageName(String search);
+    Single <List<PackageInfo>> findPackageInfoWithPackageName(String search);
 
     @Query("SELECT * FROM PackageInfo WHERE appName =:search")
-    Flowable <List<PackageInfo>> findPackageInfoWithAppName(String search);
+    Single <List<PackageInfo>> findPackageInfoWithAppName(String search);
 }
